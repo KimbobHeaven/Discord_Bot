77e2ef7d93ccc82dfe3f7789d32d3d2e77c4f04f
MinSeo <danalstj@gmail.com>
2020-12-30 17:57:07 +0900

    현재 시간 구하기

--- Bot.py
+++ Bot.py
@@ -1,26 +1,29 @@
 import discord
 import random
-import time
+import datetime
 from keep_running import keep_alive
 
-token = "" # 여기다 자신의 봇 토큰 붙여넣기
+token = '' # 여기다 자신의 봇 토큰 붙여넣기
+latest_time = {}
+latest_message = {}
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('\nLogged on as', self.user, "\n")
+        print('\nLogged on as', self.user)
     
     async def on_message(self, message):
         # don't respond to ourselves
         if message.author == self.user:
             return
         
-        message_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        author, sharp_num = str(message.author).split('#') # 이름#1234 형태를 #을 기준으로 분리
+        now = message.created_at + datetime.timedelta(hours=9) # 세계 표준시 + 9시간
+        now -= datetime.timedelta(microseconds=now.microsecond) # 마이크로초 생략
         
-        print(message.author) # 메세지 친 사람
-        print(message.created_at) # 메세지 친 시각 (세계 표준시)
-        print(message_time) # 현재 컴퓨터 시각
-        print(message.content) # 메세지 내용
         print()
+        print(author) # 메세지 친 사람
+        print(now) # 메세지 친 시각
+        print(message.content) # 메세지 내용
 
         if message.content == 'ping':
             await message.channel.send('pong')
@@ -32,13 +35,17 @@ class MyClient(discord.Client):
             await message.channel.send('점점점...')
         
         else:
-            chat = random.randrange(1,20)
-            if chat == 1:
+            ran = random.randrange(1,20)
+            if ran == 1:
                 await message.channel.send('어쩌라고')
-            elif chat == 2:
+            elif ran == 2:
                 await message.channel.send('응 아니야~')
-            elif chat == 3:
+            elif ran == 3:
                 await message.channel.send('그래서 뭐')
+
+        # 사람마다 가장 최근의 최근 메세지 보낸 시각, 메세지 내용 저장
+        latest_time[author] = now 
+        latest_message[author] = message.content
 
 client = MyClient()
 keep_alive()
@@ -54,7 +61,7 @@ client.run(token)
 # async def ping(ctx):
 #     await ctx.send('pong')
 
-# bot.run('token')
+# bot.run(token)
 
 ################################################################################
 
