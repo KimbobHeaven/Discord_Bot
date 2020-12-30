d2eb63cdbcfe07de8ecddb7a8f9b5edaac3d7bb9
MinSeo <danalstj@gmail.com>
2020-12-30 13:38:26 +0900

    봇 계속 돌아가게 해주는 코드 추가

--- Bot.py
+++ Bot.py
@@ -1,10 +1,13 @@
 import discord
 import random
 import time
+from keep_running import keep_alive
+
+token = "" # 여기다 자신의 봇 토큰 붙여넣기
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user, "\n")
+        print('\nLogged on as', self.user, "\n")
     
     async def on_message(self, message):
         # don't respond to ourselves
@@ -14,27 +17,32 @@ class MyClient(discord.Client):
         message_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         
         print(message.author) # 메세지 친 사람
-        print(message.created_at) # 메세지 친 시각
-        print(message.content) # 메세지 내용
+        print(message.created_at) # 메세지 친 시각 (세계 표준시)
         print(message_time) # 현재 컴퓨터 시각
-        
-        # if message.content == 'ping':
-        #     await message.channel.send('pong')
+        print(message.content) # 메세지 내용
+        print()
 
-        # elif message.content == '응~':
-        #     await message.channel.send('아니야~')
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+        elif message.content == '응~':
+            await message.channel.send('아니야~')
         
-        # else:
-        #     chat = random.randrange(1,4) 
-        #     if chat == 1:
-        #         await message.channel.send('어쩌라고')
-        #     elif chat == 2:
-        #         await message.channel.send('응 아니야~')
-        #     elif chat == 3:
-        #         await message.channel.send('그래서 뭐')
+        elif message.content == '...':
+            await message.channel.send('점점점...')
+        
+        else:
+            chat = random.randrange(1,20)
+            if chat == 1:
+                await message.channel.send('어쩌라고')
+            elif chat == 2:
+                await message.channel.send('응 아니야~')
+            elif chat == 3:
+                await message.channel.send('그래서 뭐')
 
 client = MyClient()
-client.run('token')
+keep_alive()
+client.run(token)
 
 ################################################################################
 
